d750c3c7eef1234bc08297181912a00e6ac37fd3
ikundaE <enoch.ikunda@gmail.com>
2023-07-13 11:21:55 -0400

    change base

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "https://github.com/ikundaE/pokedex-react",
+  base: "/pokedex-react",
   plugins: [react()],
 })
